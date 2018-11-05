19d78841da16e80151e5a651400fe17390a5cec0
Dennis van Blanken <99040363@mydavinci.nl>
2018-11-05 09:40:27 +0100

    testing

--- resources/views/todo/show.blade.php
+++ resources/views/todo/show.blade.php
@@ -2,7 +2,6 @@
 
 
 @section('content')
-    <link href="{{ asset('css/hometodo.css') }}" rel="stylesheet">
     <div class="form-group row">
         <div class="col-xs-2" id="foto">
             <h1 data-id="{{$todo->id}}" id="title">{{ $todo->name }} <img src="/imgs/pencil.png" alt="pencil" style="cursor: pointer;"></h1>
@@ -36,53 +35,45 @@
                 <div class="modal-footer">
                     <button type="button" id="close" name="close" class="btn btn-secondary" data-dismiss="modal">Close</button>
                     <button type="button" id="save" name="save" class="btn btn-primary" data-dismiss="modal">Save changes</button>
-
                 </div>
             </div>
         </div>
     </div>
 
 <hr>
-<?php if ($todoUsers) { ?>
-<div class="todoUsers">
-    <div class="todoUser"></div>
-    @foreach($todoUsers as $todoUser)
-        <div class="todoUser">
-            <p>{{ $todoUser->get()[0]->name }}<a href="/delete/todoUser/{{$todoUser->get()[0]->id}}" onclick="return confirm('Are you sure?')" class="ml-5">x</a></p>
-        </div>
-    @endforeach
-</div>
-<?php } ?>
+
 <div class="row justify-content-center">
-    <div style="margin-bottom: 20px;" class="col-md-8">
-        <div class="card">
-            <div class="card-header"><p style="margin: 7px 0;float: left;">Add user</p>
-                <form style="float: right;" method="post" action="{{ url('store/todoUser') }}">
-                    @csrf
-                    <input hidden type="text" name="todo_id" value="{{ $todo->id }}">
-                    <input style="width: 200px;display: inline-block;" type="text" class="form-control" name="user" placeholder="Add user...">
-                    <button style="display: inline-block;" type="submit" class="btn btn-success">Submit</button>
-                </form>
+        <div style="margin-bottom: 20px;" class="col-md-8">
+            <div class="card">
+                <div class="card-header"><p style="margin: 7px 0;float: left;">Add user</p>
+                    {{--<form style="float: right;" method="post" action="{{url('store/todoUser')}}">--}}
+                    <form style="float: right;" method="post" action="{{url('store/todo/addUser')}}">
+                        @csrf
+                        <input hidden type="text" name="todo_id" value="{{ $todo->id }}">
+                        <input style="width: 200px;display: inline-block;" type="text" class="form-control" name="user" placeholder="Add user...">
+                        <button style="display: inline-block;" type="submit" class="btn btn-success">Submit</button>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-md-8">
-        <div class="card">
-            <div class="card-header">Add task</div>
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Add task</div>
 
-            <div class="card-body">
-                <form method="post" action="{{url('store/todo/task')}}">
-                    @csrf
-                    <input hidden type="text" name="todo_id" value="{{ $todo->id }}">
+                <div class="card-body">
+                    <form method="post" action="{{url('store/todo/task')}}">
+                        @csrf
+                        <input hidden type="text" name="todo_id" value="{{ $todo->id }}">
 
-                    <div class="form-group col-md-4">
-                        <label for="Name">Task</label>
-                        <input type="text" class="form-control" name="content" placeholder="Add task...">
-                    </div>
-                    <div class="form-group col-md-4" style="margin-top:60px">
-                        <button type="submit" class="btn btn-success">Submit</button>
-                    </div>
-                </form>
+                        <div class="form-group col-md-4">
+                            <label for="Name">Task</label>
+                            <input type="text" class="form-control" name="content" placeholder="Add task...">
+                        </div>
+                        <div class="form-group col-md-4" style="margin-top:60px">
+                            <button type="submit" class="btn btn-success">Submit</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
@@ -91,3 +82,6 @@
 
 @endsection
 
+@section('scripts')
+    <script type="text/javascript" src="{{ asset('js/ajax.js') }}"></script>
+@endsection
